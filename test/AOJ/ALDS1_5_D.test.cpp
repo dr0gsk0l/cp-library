ad853637fc5260912cafe843864724789b6be2cd
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2023-09-28 16:28:21 +0900

    lang = ja を無くす

--- test/AOJ/ALDS1_5_D.test.cpp
+++ test/AOJ/ALDS1_5_D.test.cpp
@@ -1,12 +1,15 @@
-#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_D&lang=ja"
+#define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ALDS1_5_D"
 #include <bits/stdc++.h>
 using namespace std;
 
 #include "library/util/InversionNumber.cpp"
 
-int main(){
-  int n;cin>>n;
+int main()
+{
+  int n;
+  cin >> n;
   vector<int> v(n);
-  for(int i=0;i<n;i++)cin>>v[i];
-  cout<<inversion_number(v)<<endl;
+  for (int i = 0; i < n; i++)
+    cin >> v[i];
+  cout << inversion_number(v) << endl;
 }
